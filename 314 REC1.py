f5e35befd23c1ff801cbef82a94c12aa0fc64f96
nhattruo <74224696+nhattruo@users.noreply.github.com>
2021-09-07 18:26:40 -0400

    Add files via upload

---
+++ 314 REC1.py
@@ -0,0 +1,20 @@
+print(6%2)
+print(6**2)
+print((100%2)+1)
+x="hello"
+
+print("Hello \n World")
+
+points_obtained=30
+
+total_points=40
+
+percentage=(points_obtained / total_points)*100
+
+print(percentage)
+
+x="I am a UB student"
+
+print(x[0])
+
+print(x[7])
